a8984793f7578c927d370d314fd9ec3b043937ea
Romeo Mondeke <2792089m@student.gla.ac.uk>
2025-03-28 01:12:00 +0000

    Added Stock History Page

--- investment_manager_main/models.py
+++ investment_manager_main/models.py
@@ -116,13 +116,3 @@ class ContactMessage(models.Model):
 
     def __str__(self):
         return f"Message from {self.name} ({self.email})"
-
-class ContactMessage(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20, blank=True)
-    message = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Message from {self.name} ({self.email})"
